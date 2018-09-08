6bacea4c23b2deeb1925f3b3ea1b427640fab536
Aleksandr Kochkurkin <mailto:89636617290@mail.ru>
2018-09-09 00:04:07 +0200

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
+/**
+* Class Calculate
+* @author Aleksandr 
+* @since 08.09.2018
+* @version 1
+*/
 
 public class Calculate{
+	
+/**
+* Конструктор, вывод строки в консоль
+* @param args - args.
+*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
