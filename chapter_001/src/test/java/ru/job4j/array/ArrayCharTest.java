486295b15f2ee1ba4a1f08befa88b877a638693b
Aleksandr Kochkurkin <mailto:89263693878@mail.ru>
2019-02-13 22:31:57 +0300

    6.4. Слово начинается с ... [#41585]

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Aleksandr Kochkurkin (mailto:info@debitop.com)
+ * @version 1.0
+ * @since 13.02.2019
+ */
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
